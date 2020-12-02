93f1edc48d50b28422f1b6a44c36eb261c19a074
tjohanne42 <theo.johannet@gmail.com>
2020-12-02 11:08:06 +0100

    change car

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import pygame
 import vehicles as vh
 
-
-def step(pos, key):
+def step(key):
     if key == "up":
-        pos[1] -= 10
+        allVehicles.vehicles[index[0]].y -= 10
     elif key == "down":
-        pos[1] += 10
+         allVehicles.vehicles[index[0]].y += 10
     elif key == "right":
-        pos[0] += 10
+        allVehicles.vehicles[index[0]].x += 10
     elif key == "left":
-        pos[0] -= 10
-    return pos
+        allVehicles.vehicles[index[0]].x -= 10
+    if key == "+index[0]":
+    	index[0] += 1
+    elif key == "-index[0]":
+    	index[0] -= 1
 
-
-def display(pos):
+def display():
     # Color entire window with a certain color.
     window_surface.fill((255, 255, 255))
 
@@ -27,11 +28,10 @@ def display(pos):
     window_surface.blit(size_example_txt, (100, 225))
     window_surface.blit(manuel_txt, (100, 275))
     # Load one vehicule
-    window_surface.blit(allVehicles.getListVehicules()[0].getImage(), (0, 0))
+    window_surface.blit(allVehicles.getListVehicules()[index[0]].getImage(), (allVehicles.vehicles[index[0]].x, allVehicles.vehicles[index[0]].y))
 
     # Blit the surface "button_surface" on the main surface (window_surface) on coord x,y = 100,75.
     # For exemple we can control it.
-    window_surface.blit(button_surface, (pos[0], pos[1]))
 
     # Applies new changes on the display.
     pygame.display.flip()
@@ -59,6 +59,7 @@ font_lemonmilk = pygame.font.Font('assets/LEMONMILK-Regular.otf', 20)
 allVehicles = vh.Vehicles()
 for vehicle in allVehicles.getListVehicules():
     print(vehicle)
+index = [0]
 
 # clock is for fps
 clock = pygame.time.Clock()
@@ -66,11 +67,8 @@ clock = pygame.time.Clock()
 # Allow to hold the same key (arg in ms)
 pygame.key.set_repeat(1, 1)
 
-# Pos of button for example
-pos = [0, 0]
-
 # First display.
-display(pos)
+display()
 
 # Start mainloop.
 launched = True
@@ -87,14 +85,18 @@ while launched:
             if event.key == pygame.K_a:
                 print(event.key)
             elif event.key == pygame.K_UP:
-                pos = step(pos, "up")
+                step("up")
             elif event.key == pygame.K_DOWN:
-                pos = step(pos, "down")
+                step("down")
             elif event.key == pygame.K_LEFT:
-                pos = step(pos, "left")
+                step("left")
             elif event.key == pygame.K_RIGHT:
-                pos = step(pos, "right")
-            display(pos)
+                step("right")
+            elif event.key == pygame.K_c:
+                step("+index[0]")
+            elif event.key == pygame.K_v:
+                step("-index[0]")
+            display()
 
     # For fps.
     clock.tick(60)

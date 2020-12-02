e1557f6f0162560d33d70e76cf2becade1f4a3d1
Sacha <sgryman@yahoo.fr>
2020-12-02 17:23:15 +0100

    Update Roads.py
    This is the good one

--- Roads.py
+++ Roads.py
@@ -11,8 +11,9 @@ class Roads:
         self.roadGreen = pygame.image.load('assets/Roads/Green.jpg')
         self.roadPass = pygame.image.load('assets/Roads/RoadPass.jpg')
         self.roadBorder = pygame.image.load('assets/Roads/RoadNoPass.jpg')
+        self.offsetRow = -self.surfaceSize
 
-    def display(self, speed):
+    def display(self):
 
         for col in range(0, self.window_width, self.surfaceSize):
             for row in range(0, self.window_height, self.surfaceSize):
@@ -25,16 +26,19 @@ class Roads:
                 else:
                     self.window_surface.blit(self.roadPass, (col, row))
 
-
-    def display2(self, speed):
+    def display(self, speed):
 
         for col in range(0, self.window_width, self.surfaceSize):
-            for row in range(0, self.window_height, self.surfaceSize):
+            for row in range(self.offsetRow, self.window_height, self.surfaceSize):
                 self.window_surface.blit(self.roadGreen, (col, row))
 
         for col in range(self.surfaceSize, self.window_width - self.surfaceSize, self.surfaceSize):
-            for row in range(0, self.window_height, self.surfaceSize):
+            for row in range(self.offsetRow, self.window_height, self.surfaceSize):
                 if col == self.surfaceSize * 1 or col == self.surfaceSize * 4:
                     self.window_height.blit(self.roadBorder, (col, row))
                 else:
                     self.window_surface.blit(self.roadPass, (col, row))
+
+        self.offsetRow += speed
+        if self.offsetRow >= 0:
+            self.offsetRow = -self.surfaceSize
